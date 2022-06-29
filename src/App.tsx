07edc8443f2d8058f00c754421b2ac6d1ed496ee
Vitor Galeti <vitor.fgaletti@gmail.com>
2022-06-29 10:54:34 -0300

    :sparkles: Create tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Header } from './components/Header';
 import { TaskList } from './components/TaskList';
 
-import styles from './App.module.css';
 import './global.css';
 
 function App() {
   return (
-    <div className={styles.wrapper}>
+    <div>
       <Header />
       <TaskList />
     </div>
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,15 +1,56 @@
 import { PlusCircle, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+('uuid');
 
 import styles from './TaskList.module.css';
 
+interface TaskListProps {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
 export function TaskList() {
-  const isCompleted = true;
+  const [newCreateTask, setNewCreateTask] = useState('');
+  const [tasks, setTasks] = useState<TaskListProps[]>([]);
+
+  function handleCreateNewTask() {
+    const newTask = [
+      ...tasks,
+      { id: uuidv4(), title: newCreateTask, isCompleted: false }
+    ];
+    setTasks(newTask);
+    setNewCreateTask('');
+  }
+
+  function handleCompletedTask(id: string) {
+    const newTask = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTask);
+  }
+
+  function deleteTask(id: string) {
+    const newTask = tasks.filter(task => task.id !== id);
+    setTasks(newTask);
+  }
+
+  const countCompletedTask = tasks.filter(task => task.isCompleted).length;
 
   return (
     <section className={styles.section}>
       <div className={styles.inputGroup}>
-        <input type="text" placeholder="Adicione uma nova tarefa" />
-        <button type="button">
+        <input
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+          value={newCreateTask}
+          onChange={e => setNewCreateTask(e.target.value)}
+        />
+        <button type="button" onClick={handleCreateNewTask}>
           Criar
           <PlusCircle size={16} />
         </button>
@@ -18,30 +59,50 @@ export function TaskList() {
       <main>
         <header className={styles.headerTask}>
           <strong className={styles.createdTasks}>
-            Tarefas criadas <span>0</span>
+            Tarefas criadas <span>{tasks.length}</span>
           </strong>
           <strong className={styles.completedTasks}>
-            Concluídas <span>2 de 5</span>
+            Concluídas
+            <span>
+              {tasks.length === 0
+                ? 0
+                : `${countCompletedTask} de ${tasks.length}`}
+            </span>
           </strong>
         </header>
 
-        <ul
-          className={`${styles.tasks} ${isCompleted ? styles.completed : ''}`}
-        >
-          <li>
-            <label className={styles.checkboxContainer}>
-              <input type="checkbox" />
-              <span className={styles.checkmark}></span>
-            </label>
+        {tasks.length === 0 && (
+          <div className={styles.tasksNotCreated}>
+            <img src="./src/assets/img/Clipboard.png" alt="Prancheta" />
+            <strong>Você ainda não tem tarefas cadastradas</strong>
+            <span>Crie tarefas e organize seus itens a fazer</span>
+          </div>
+        )}
 
-            <p>
-              Integer urna interdum massa libero auctor neque turpis turpis
-              semper. Duis vel sed fames integer.
-            </p>
-            <button type="button">
-              <Trash size={24} />
-            </button>
-          </li>
+        <ul className={styles.tasks}>
+          {tasks.map(task => (
+            <li key={task.id}>
+              <label className={styles.checkboxContainer}>
+                <input
+                  type="checkbox"
+                  onClick={() => handleCompletedTask(task.id)}
+                />
+                <span className={styles.checkmark}></span>
+              </label>
+              <div
+                className={
+                  task.isCompleted
+                    ? `${styles.taskContent} ${styles.completed}`
+                    : `${styles.taskContent}`
+                }
+              >
+                <p>{task.title}</p>
+                <button type="button" onClick={() => deleteTask(task.id)}>
+                  <Trash size={24} />
+                </button>
+              </div>
+            </li>
+          ))}
         </ul>
       </main>
     </section>
